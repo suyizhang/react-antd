0ebea4a8b92f72e35888209ce1be657420a3eae3
zhangchenxing <zhangchenxing@seemmo.com>
2019-08-30 15:42:40 +0800

    add decorator

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, addLessLoader, fixBabelImports } = require('customize-cra');
+const { override, addLessLoader, fixBabelImports, addDecoratorsLegacy } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
@@ -9,5 +9,6 @@ module.exports = override(
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: { '@primary-color': '#1DA57A' }
-  })
+  }),
+  addDecoratorsLegacy()
 );
